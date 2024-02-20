0adda18c79850d38a112eaa463270bcb30e00b1a
Flavio De Stefano <destefano.flavio@gmail.com>
2024-02-20 09:17:16 +0100

    Removing pug

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,10 @@ import express from "express";
 import logger from "morgan";
 import * as path from "path";
 
-import { errorHandler, errorNotFoundHandler } from "./middlewares/errorHandler";
-
-// Routes
-import { index } from "./routes/index";
-// Create Express server
 export const app = express();
 
 // Express configuration
-app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "pug");
-
+app.set("port", process.env.VIRTUAL_PORT || 3000);
 app.use(logger("dev"));
 
 app.use(express.static(path.join(__dirname, "../public")));
-app.use("/", index);
-
-app.use(errorNotFoundHandler);
-app.use(errorHandler);
