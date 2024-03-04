ab06599c85af71be3f487435e1647bb0caa611a7
Flavio De Stefano <destefano.flavio@gmail.com>
2024-03-04 14:11:23 +0100

    Simple game

--- src/client/index.ts
+++ src/client/index.ts
@@ -8,13 +8,18 @@ const client = mqtt.connect({
     clientId: "elrojobotones-js",
 });
 
-const $buzzer = document.querySelector("#buzzer") as HTMLAudioElement;
+const $buzzer_left = document.querySelector("#buzzer-left") as HTMLAudioElement;
+const $buzzer_right = document.querySelector(
+    "#buzzer-right",
+) as HTMLAudioElement;
+
 const $connection_status_buttons = document.querySelector(
     "#connection-status-buttons",
 ) as HTMLSpanElement;
 const $connection_status_server = document.querySelector(
     "#connection-status-server",
 ) as HTMLSpanElement;
+const $sentence = document.querySelector("#sentence") as HTMLDivElement;
 
 const $left_button = document.querySelector(
     "#left-button",
@@ -40,7 +45,6 @@ function sendMessageToButtons(method: string, data: Record<string, any> = {}) {
 }
 
 client.on("connect", () => {
-    console.log("Connected to MQTT broker");
     $connection_status_server.textContent = "Connected";
     $connection_status_server.classList.add("success");
 
@@ -49,25 +53,75 @@ client.on("connect", () => {
     client.subscribe(`${NAMESPACE}/press`);
 
     // Ping the buttons to check their availability
-    client.publish(
-        `${NAMESPACE}/callback`,
-        JSON.stringify({
-            command: "availability",
-        }),
-    );
+    // client.publish(
+    //     `${NAMESPACE}/callback`,
+    //     JSON.stringify({
+    //         command: "availability",
+    //     }),
+    // );
 });
 
+const score = {
+    L: 0,
+    R: 0,
+};
+
+let intv: ReturnType<typeof setTimeout> | null = null;
+let quizStarted = false;
+let pressIntv: ReturnType<typeof setTimeout> | null = null;
+
 function whenClientButtonPressed(button: "L" | "R") {
-    $buttons[button].classList.add("active");
+    if (intv) clearTimeout(intv);
+
+    if (quizStarted) {
+        score[button]++;
+        $buttons[button].classList.add("success");
+        $buttons[button == "L" ? "R" : "L"].classList.add("late");
+    } else {
+        score[button] -= 2;
+        $buttons[button].classList.add("wrong");
+        $buttons[button == "L" ? "R" : "L"].classList.add("other_is_wrong");
+    }
+
+    for (const key in $buttons) {
+        $buttons[key]!.textContent = score[key];
+    }
+
+    setTimeout(() => {
+        onReset();
+    }, 3000);
 }
 
 function onReset() {
+    quizStarted = false;
     sendMessageToButtons("reset");
 
-    $left_button.classList.remove("active");
-    $right_button.classList.remove("active");
-}
+    $left_button.className = "button";
+    $right_button.className = "button";
 
+    // Generate random character in the sentence
+    const text = [];
+    for (let i = 0; i < 500; i++) {
+        text.push(String.fromCharCode(65 + Math.floor(Math.random() * 26)));
+    }
+    $sentence.textContent = text.join(" ");
+
+    if (intv) clearTimeout(intv);
+    if (pressIntv) clearTimeout(pressIntv);
+    intv = setTimeout(() => {
+        quizStarted = true;
+
+        pressIntv = setTimeout(() => {
+            onReset();
+        }, 3_000);
+
+        // Change a random letter in s $sentence
+        const index = Math.floor(Math.random() * text.length);
+        text[index] = String.fromCharCode(65 + Math.floor(Math.random() * 26));
+        $sentence.textContent = text.join(" ");
+    }, Math.random() * 10_000);
+}
+``;
 client.on("message", (topic, message) => {
     console.log(`Received message on topic: ${topic}`, message.toString());
 
@@ -88,8 +142,13 @@ client.on("message", (topic, message) => {
             break;
 
         case `${NAMESPACE}/press`:
-            $buzzer.currentTime = 0;
-            $buzzer.play();
+            if (message.toString() === "L") {
+                $buzzer_left.currentTime = 0;
+                $buzzer_left.play();
+            } else if (message.toString() === "R") {
+                $buzzer_right.currentTime = 0;
+                $buzzer_right.play();
+            }
             break;
 
         default:
